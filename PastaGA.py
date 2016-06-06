4ccc63067b5cd5f4673a40579273742a22113a03
GastricFluid <comeaupaul98@gmail.com>
2016-06-06 15:55:54 -0600

    Add files via upload

---
+++ PastaGA.py
@@ -0,0 +1,17 @@
+import random
+
+base_pop = 100
+generations = 1000
+pasta_book = {}
+ingredient_list = []
+
+def generate_basepop(pop_level, ingredient_list):
+    for chrom in range(0, pop_level):
+
+    return
+
+def fitness_check(chromosome):
+    #determines how delicious pasta is based off of genes(ingredients)
+    return
+
+generate_basepop(base_pop, ingredient_list)
