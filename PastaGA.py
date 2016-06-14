0d4c929afb0fc0617488dedb9d51ba56969567dd
GastricFluid <comeaupaul98@gmail.com>
2016-06-14 11:03:18 -0600

    functional apart from no population slimming and issues with selecting the same parent twice

--- PastaGA.py
+++ PastaGA.py
@@ -5,7 +5,7 @@ import operator
 import sys
 import argparse
 
-mutate_chance = 2
+mutate_chance = 90
 base_pop = 100
 generation_max = 100
 
@@ -23,14 +23,14 @@ class pasta:
     def __init__ (self, gene_1, gene_2, gene_3, gene_4):
         #creates object from 4 genes
 
-        pasta.gene_1 = gene_1
-        pasta.gene_2 = gene_2
-        pasta.gene_3 = gene_3
-        pasta.gene_4 = gene_4
+        self.__gene_1 = gene_1
+        self.__gene_2 = gene_2
+        self.__gene_3 = gene_3
+        self.__gene_4 = gene_4
 
     def list_ing(self):
         #gives a list of the genes
-        return [self.gene_1, self.gene_2, self.gene_3, self.gene_4]
+        return [self.__gene_1, self.__gene_2, self.__gene_3, self.__gene_4]
 
     def set_fitvalue(self):
         #use to set fitvalue
@@ -82,15 +82,19 @@ class Chrom_Population:
             if def_check == 1:
                 break
         def_check = 0
+        prev_parent = None
         for chrom in prob_dic2:
             if prob_dic2[chrom] >= parentchance_2:
                 parent_2 = chrom
-                if parent_2 == parent_1:
-
-                    parent_2 = next(chrom)
+                #if parent_2 == parent_1:
+                    #if prev_parent != None:
+                        #parent_2 = prev_parent
+                    #if prev_parent == None:
+                        #parent_2 = next(chrom)
                 def_check = 1
             if def_check == 1:
                 break
+            prev_parent = chrom
         parent_tuple = (parent_1, parent_2)
         return parent_tuple
 
@@ -108,8 +112,8 @@ class Chrom_Population:
         ingredients_2 = pasta2.list_ing()
         ingredients_3 = []
         mutated_child = []
-        for num in range(0,4):
-            random_layout = random.randint(0,6)
+        for num in range(0,1):
+            random_layout = random.randint(1, 6)
             if random_layout == 1:
                 ingredients_3 = [ingredients_1[0],ingredients_1[1], ingredients_2[2], ingredients_2[3]]
             if random_layout == 2:
@@ -133,6 +137,7 @@ class Chrom_Population:
     def reset_population(self):
         #replaces current dict with new generated genes/use to create initial population
         pasta_count = 0
+        prev_pasta = pasta("spaghetti", "a", "a", "a")
         for chrom in range(0, base_pop):
             pasta_count += 1
             pasta_name = pasta_count
@@ -150,15 +155,22 @@ class Chrom_Population:
 
     def give_most_fit(self):
         #returns fitteset chromosome
-        return max(self.chrom_dict.iteritems(), key=operator.itemgetter(1))[0]
+        return max(self.__chrom_fitdict.iteritems(), key=operator.itemgetter(1))[0]
 
 def main():
     #parse line arguments
-    first_gen = Chrom_Population([])
-    first_gen.reset_population()
-    for gens in generation_max:
+    population = []
+    for gens in range(0, generation_max):
+        gens = Chrom_Population(population)
+        if population == []:
+            gens.reset_population()
+        gens.dict_producer()
+        gens.probability_parent()
+        for pairs in range(0, base_pop/2):
+            gens.breed_et_mutate(gens.parent_select())
+        population = gens.get_generation()
+        print gens.give_most_fit()
 
-        pass
 
 
 
