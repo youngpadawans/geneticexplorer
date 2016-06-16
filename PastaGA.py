d2ee8c965855457d940e8c3a2d3d6b29025462d0
GastricFluid <comeaupaul98@gmail.com>
2016-06-16 15:42:25 -0600

    completed most of what the comments covered, may still be rough around the edges

--- PastaGA.py
+++ PastaGA.py
@@ -7,24 +7,31 @@ import sys
 from sys import argv
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-mutate_chance = 2
-base_pop = 10
-generation_max = 10
 
-poss_ingredients = ["tomato sauce","pesto","mozzarella","alfredo sauce", "pasta shells","chile sauce", "pasta bowties", "parmesan","meatballs","pasta tubes","shredded chicken","basil", "spinach","chives","shrimp","mushroom","macaroni","feta cheese","noodles","sausage","olives"]
+poss_ingredients = ["tomato sauce","pesto","mozzarella","alfredo sauce", "pasta shells","chile sauce", "pasta bowties",
+                    "parmesan","meatballs","pasta tubes","shredded chicken","basil", "spinach","chives","shrimp",
+                    "mushroom","macaroni","feta cheese","noodles","sausage","olives"]
 
 class Gene:
+    """formatting for the object of a single ingredient
+    """
 
     __ingredient = ""
-    score_dict = {"tomato sauce" : 7,"pesto" : 5,"mozzarella" : 5,"alfredo sauce" : 10, "pasta shells" : 7,"chile sauce" : 5, "pasta bowties" : 5, "parmesan" : 10,"meatballs" : 8,"pasta tubes" : 10,"shredded chicken" : 10,"basil" : 7, "spinach" : 10,"chives" : 2,"shrimp" : 2,"mushroom" : 2,"macaroni" : 5,"feta cheese" : 2,"noodles" : 7,"sausage" : 7,"olives": 0}
+    score_dict = {"tomato sauce" : 7,"pesto" : 5,"mozzarella" : 5,"alfredo sauce" : 10, "pasta shells" : 7,"chile sauce" : 5,
+                  "pasta bowties" : 5, "parmesan" : 10,"meatballs" : 8,"pasta tubes" : 10,"shredded chicken" : 10,"basil" : 7,
+                  "spinach" : 10,"chives" : 2,"shrimp" : 2,"mushroom" : 2,"macaroni" : 5,"feta cheese" : 2,"noodles" : 7,
+                  "sausage" : 7,"olives": 0}
 
     def __init__(self, ingredient):
+        """creates object from single ingredient
+        """
 
         self.__ingredient = ingredient
 
 
 class PastaDish:
-    #format and functions of the pasta chromosome
+    """format and functions of the pasta chromosome
+    """
 
     __gene_1 = ""
     __gene_2 = ""
@@ -33,7 +40,8 @@ class PastaDish:
     fitvalue = 0
 
     def __init__ (self, gene_1, gene_2, gene_3, gene_4):
-        #creates object from 4 genes
+        """creates object from 4 genes
+        """
 
         self.__gene_1 = gene_1
         self.__gene_2 = gene_2
@@ -41,10 +49,15 @@ class PastaDish:
         self.__gene_4 = gene_4
 
     def list_ing(self):
-        #gives a list of the genes
+        """gives a list of the genes
+        """
+
         return [self.__gene_1, self.__gene_2, self.__gene_3, self.__gene_4]
 
     def set_fitvalue(self):
+        """sets the fitness value of a chromosome
+        """
+
         fit_val = 0
         fit_val += Gene.score_dict[self.__gene_1]
         fit_val += Gene.score_dict[self.__gene_2]
@@ -52,21 +65,23 @@ class PastaDish:
         fit_val += Gene.score_dict[self.__gene_4]
         self.fitvalue = fit_val
 
-    def get_fitvalue(self):
-        #gives the set fitvalue
-        return self.fitvalue
-
 class ChromPopulation:
-
+    """format and fuctions of a population of chromosomes
+    """
     __chrom_list = []
     __chrom_fitdict = {}
     __prob_dict = {}
 
     def __init__(self, chrom_list):
+        """the given list defines the chomosome population
+        """
+
         self.__chrom_list = chrom_list
 
     def cull(self):
-        #remove 10% of self
+        """remove 10% of self
+        """
+
         count_rem = (len(self.__chrom_list)/10)
         execution =reversed(Counter(self.__chrom_fitdict).most_common()[-count_rem:])
         for object in execution:
@@ -74,7 +89,9 @@ class ChromPopulation:
         pass
 
     def probability_parent(self):
-        #creates a dictionary containing the probability of being selected as a parent
+        """creates a dictionary containing the probability of being selected as a parent
+        """
+
         self.__prob_dict = {}
         total = 0
         prev_chance = 0
@@ -89,7 +106,8 @@ class ChromPopulation:
         pass
 
     def parent_select(self):
-        #selects parents based on probability dict
+        """selects parents based on probability dict
+        """
         parentchance_1 = random.random()
         parentchance_2 = random.random()
         prob_dic1 = self.__prob_dict
@@ -114,13 +132,17 @@ class ChromPopulation:
         return parent_tuple
 
     def dict_producer(self):
+        """creates a dictionary containing pasta types and their fitness value
+        """
+
         for chrom in self.__chrom_list:
             chrom.set_fitvalue()
-            self.__chrom_fitdict[chrom] = chrom.get_fitvalue()
+            self.__chrom_fitdict[chrom] = chrom.fitvalue
         pass
 
-    def breed_et_mutate(self, couple_tuple):
-        #mutates children after bred from two chromosomes
+    def breed_et_mutate(self, couple_tuple, mutate_chance):
+        """mutates children after bred from two chromosomes
+        """
         pasta1 = couple_tuple[0]
         pasta2 = couple_tuple[1]
         ingredients_1 = pasta1.list_ing()
@@ -149,8 +171,9 @@ class ChromPopulation:
         child = PastaDish(mutated_child[0], mutated_child[1], mutated_child[2], mutated_child[3])
         self.__chrom_list.append(child)
 
-    def reset_population(self):
-        #replaces current dict with new generated genes/use to create initial population
+    def reset_population(self, base_pop):
+        """replaces current dict with new generated genes/use to create initial population
+        """
         pasta_count = 0
         for chrom in range(0, base_pop):
             pasta_count += 1
@@ -164,52 +187,67 @@ class ChromPopulation:
         pass
 
     def get_generation(self):
-        #returns the generation dict
+        """returns the generation dict
+        """
         return self.__chrom_list
 
     def give_most_fit(self):
-        #returns fitteset chromosome
+        """returns fitteset chromosome
+        """
         return max(self.__chrom_fitdict.iteritems(), key=operator.itemgetter(1))[0]
 
 def command_line(arg_list=argv):
-
+    """defines command line arguments
+    """
     options_parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 
-    options_parser.add_argument('--mutate_chance', help = 'percentage chance of every gene to mutate')
-    options_parser.add_argument('--base_population', help = 'amount of chromosomes the GA generates to as a base population')
-    options_parser.add_argument('--maximum_generation', help = 'amount of times the GA goes through its cycle')
+    options_parser.add_argument('-m', help = 'percentage chance of every gene to mutate', action = "store" ,dest = "percent" , type=int, default= 2)
+    options_parser.add_argument('-b', help = 'amount of chromosomes the GA generates to as a base population', action = "store" ,dest = "population" , type=int, default=10)
+    options_parser.add_argument('-g', help = 'amount of times the GA goes through its cycle', action = "store" ,dest = "cycle" , type=int, default=10)
 
     opts, unkown = options_parser.parse_known_args(args = arg_list)
 
     return opts
 
 def main(args, args_parsed = None):
+    """defines variables based off of command line arguments
+    """
     if args_parsed is not None:
         opts = args_parsed
 
     else:
         opts = command_line(args)
 
-    workflow()
+    if opts.cycle > 0:
+        generation_max = opts.cycle
 
-def workflow():
-    #parse line arguments
+    if opts.population > 0:
+        base_pop = opts.population
+
+    if opts.percent >= 0:
+        mutate_chance = opts.percent
+
+    workflow(generation_max, mutate_chance, base_pop)
+
+def workflow(generation_max, mutation_chance, base_pop):
+    """the main process which uses the class outlines and processes to run a genetic algorithm
+    """
     population = []
     for gens in range(0, generation_max):
         gens = ChromPopulation(population)
         if population == []:
-            gens.reset_population()
+            gens.reset_population(base_pop)
         gens.dict_producer()
         gens.probability_parent()
         gens.cull()
         for pairs in range(0, base_pop/2):
-            gens.breed_et_mutate(gens.parent_select())
+            gens.breed_et_mutate(gens.parent_select(), mutation_chance)
         population = gens.get_generation()
         current_best = gens.give_most_fit()
     print current_best.list_ing()
-    print current_best.get_fitvalue()
+    print current_best.fitvalue
 
 
 
 if __name__ == "__main__":
-    main()
+    main(argv)
