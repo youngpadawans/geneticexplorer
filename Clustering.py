9a464aea196d84f5c29df3b98cff1a06eba169e0
root <root@compute1.(none)>
2016-07-26 10:25:46 -0600

    added the comparisons for all the characteristics

--- Clustering.py
+++ Clustering.py
@@ -8,6 +8,7 @@ simple_sim_dict = {}
 
 #I may have to do some major rethinking of how I want to process the gene data in order to cluster it, my current plan may get out of hand.
 
+
 def gene_produce(gene_list):
     for gene in gene_list:
         gene = Gene(stuff)
@@ -105,97 +106,194 @@ def total_length(genes1, genes2):
 
 
 def TrackLength_percenetsimilar(genes1, genes2):
-
-    pass
-
-
-def StandardDeviation_percenetsimilar(gene1, gene2):
-
-    pass
-
-
-def Exontracklength_percenetsimilar(gene1, gene2):
-    pass
+    track1 = genes1.get_trackperc()
+    track2 = genes2.get_trackperc()
+    if track1 >= track2:
+        sim_score = track1 - track2
+    if track2 > track1:
+        sim_score = track2 - track1
+    return sim_score
 
 
-def StandardDeviationExontrack_percenetsimilar(gene1, gene2):
-
-    pass
-
-
-def Introntracklength_percenetsimilar(gene1, gene2):
-
-    pass
-
-
-def StandardDeviationIntrontrack_percenetsimilar(gene1, gene2):
-
-    pass
+def StandardDeviation_percenetsimilar(genes1, genes2):
+    stdevtrack1 = genes1.get_stdevtrackperc()
+    stdevtrack2 = genes2.get_stdevtrackperc()
+    if stdevtrack1 >= stdevtrack2:
+        sim_score = stdevtrack1 - stdevtrack2
+    if stdevtrack2 > stdevtrack1:
+        sim_score = stdevtrack2 - stdevtrack1
+    return sim_score
 
 
-def BaseAtrack_percenetsimilar(gene1, gene2):
-
-    pass
-
-
-def BaseCtrack_percenetsimilar(gene1, gene2):
-
-    pass
-
-
-def BaseGtrack_percenetsimilar(gene1, gene2):
-
-    pass
+def Exontracklength_percenetsimilar(genes1, genes2):
+    exontrack1 = genes1.get_exontrackperc()
+    exontrack2 = genes2.get_exontrackperc()
+    if exontrack1 >= exontrack2:
+        sim_score = exontrack1 - exontrack2
+    if exontrack2 > exontrack1:
+        sim_score = exontrack2 - exontrack1
+    return sim_score
 
 
-def BaseTtrack_percenetsimilar(gene1, gene2):
+def StandardDeviationExontrack_percenetsimilar(genes1, genes2):
+    stdevexontrack1 = genes1.get_stdevexontrackperc()
+    stdevexontrack2 = genes2.get_stdevexontrackperc()
+    if stdevexontrack1 >= stdevexontrack2:
+        sim_score = stdevexontrack1 - stdevexontrack2
+    if stdevexontrack2 > stdevexontrack1:
+        sim_score = stdevexontrack2 - stdevexontrack1
+    return sim_score
 
-    pass
+
+def Introntracklength_percenetsimilar(genes1, genes2):
+    introntrack1 = genes1.get_introntrackperc()
+    introntrack2 = genes2.get_introntrackperc()
+    if introntrack1 >= introntrack2:
+        sim_score = introntrack1 - introntrack2
+    if introntrack2 > introntrack1:
+        sim_score = introntrack2 - introntrack1
+    return sim_score
+
+
+def StandardDeviationIntrontrack_percenetsimilar(genes1, genes2):
+    stdeintrontrack1 = genes1.get_stdevexontrackperc()
+    stdeintrontrack2 = genes2.get_stdevexontrackperc()
+    if stdeintrontrack1 >= stdeintrontrack2:
+        sim_score = stdeintrontrack1 - stdeintrontrack2
+    if stdeintrontrack2 > stdeintrontrack1:
+        sim_score = stdeintrontrack2 - stdeintrontrack1
+    return sim_score
+
+
+def BaseAtrack_percenetsimilar(genes1, genes2):
+    baseA1 = genes1.get_BaseAperc()
+    baseA2 = genes2.get_BaseAperc()
+    if baseA1 >= baseA2:
+        sim_score = baseA1 - baseA2
+    if baseA2 > baseA1:
+        sim_score = baseA2 - baseA1
+    return sim_score
+
+
+def BaseCtrack_percenetsimilar(genes1, genes2):
+    baseC1 = genes1.get_BaseCperc()
+    baseC2 = genes2.get_BaseCperc()
+    if baseC1 >= baseC2:
+        sim_score = baseC1 - baseC2
+    if baseC2 > baseC1:
+        sim_score = baseC2 - baseC1
+    return sim_score
+
+
+def BaseGtrack_percenetsimilar(genes1, genes2):
+    baseG1 = genes1.get_BaseGperc()
+    baseG2 = genes2.get_BaseGperc()
+    if baseG1 >= baseG2:
+        sim_score = baseG1 - baseG2
+    if baseG2 > baseG1:
+        sim_score = baseG2 - baseG1
+    return sim_score
+
+
+def BaseTtrack_percenetsimilar(genes1, genes2):
+    BaseT1 = genes1.get_BaseTperc()
+    BaseT2 = genes2.get_BaseTperc()
+    if BaseT1 >= BaseT2:
+        sim_score = BaseT1 - BaseT2
+    if BaseT2 > BaseT1:
+        sim_score = BaseT2 - BaseT1
+    return sim_score
 
 
 def eighteenmer_percentsimilar(genes1, genes2):
-
-    pass
+    eighteenmer1 = genes1.get_eighteenmerperc()
+    eighteenmer2 = genes2.get_eighteenmerperc()
+    if eighteenmer1 >= eighteenmer2:
+        sim_score = eighteenmer1 - eighteenmer2
+    if eighteenmer2 > eighteenmer1:
+        sim_score = eighteenmer2 - eighteenmer1
+    return sim_score
 
 
 def avgexon_length(genes1, genes2):
-
-    pass
+    avgexonlen1 = genes1.get_avgexonperc()
+    avgexonlen2 = genes2.get_avgexonperc()
+    if avgexonlen1 >= avgexonlen2:
+        sim_score = avgexonlen1 - avgexonlen2
+    if avgexonlen2 > avgexonlen1:
+        sim_score = avgexonlen2 - avgexonlen1
+    return sim_score
 
 
 def avgintron_length(genes1, genes2):
-
-    pass
-
-
-def Percentintronic_percentsimilar(gene1, gene2):
-
-    pass
-
-
-def Percentexonic_percentsimilar(gene1, gene2):
-
-    pass
+    avgintronlen1 = genes1.get_avgintronperc()
+    avgintronlen2 = genes2.get_avgintronperc()
+    if avgintronlen1 >= avgintronlen2:
+        sim_score = avgintronlen1 - avgintronlen2
+    if avgintronlen2 > avgintronlen1:
+        sim_score = avgintronlen2 - avgintronlen1
+    return sim_score
 
 
-def StandardDeviationExon_percenetsimilar(gene1, gene2):
-
-    pass
-
-
-def StandardDeviationIntron_percenetsimilar(gene1, gene2):
-
-    pass
-
-
-def AverageExon_percenetsimilar(gene1, gene2):
-
-    pass
+def Percentintronic_percentsimilar(genes1, genes2):
+    percentintron1 = genes1.get_percentintronperc()
+    percentintron2 = genes2.get_percentintronperc()
+    if percentintron1 >= percentintron2:
+        sim_score = percentintron1 - percentintron2
+    if percentintron2 > percentintron1:
+        sim_score = percentintron2 - percentintron1
+    return sim_score
 
 
-def AverageIntron_percenetsimilar(gene1, gene2):
+def Percentexonic_percentsimilar(genes1, genes2):
+    percentexon1 = genes1.get_percentexonperc()
+    percentexon2 = genes2.get_percentexonperc()
+    if percentexon1 >= percentexon2:
+        sim_score = percentexon1 - percentexon2
+    if percentexon2 > percentexon1:
+        sim_score = percentexon2 - percentexon1
+    return sim_score
 
-    pass
+
+def StandardDeviationExon_percenetsimilar(genes1, genes2):
+    stdevexon1 = genes1.get_stdevexonsperc()
+    stdevexon2 = genes2.get_stdevexonsperc()
+    if stdevexon1 >= stdevexon2:
+        sim_score = stdevexon1 - stdevexon2
+    if stdevexon2 > stdevexon1:
+        sim_score = stdevexon2 - stdevexon1
+    return sim_score
+
+
+def StandardDeviationIntron_percenetsimilar(genes1, genes2):
+    stdevintron1 = genes1.get_stdevintronsperc()
+    stdevintron2 = genes2.get_stdevintronsperc()
+    if stdevintron1 >= stdevintron2:
+        sim_score = stdevintron1 - stdevintron2
+    if stdevintron2 > stdevintron1:
+        sim_score = stdevintron2 - stdevintron1
+    return sim_score
+
+
+def AverageExon_percenetsimilar(genes1, genes2):
+    averageexon1 = genes1.get_averageexonperc()
+    averageexon2 = genes2.get_averageexonperc()
+    if averageexon1 >= averageexon2:
+        sim_score = averageexon1 - averageexon2
+    if averageexon2 > averageexon1:
+        sim_score = averageexon2 - averageexon1
+    return sim_score
+
+
+def AverageIntron_percenetsimilar(genes1, genes2):
+    averageintron1 = genes1.get_averageintronperc()
+    averageintron2 = genes2.get_averageintronperc()
+    if averageintron1 >= averageintron2:
+        sim_score = averageintron1 - averageintron2
+    if averageintron2 > averageintron1:
+        sim_score = averageintron2 - averageintron1
+    return sim_score
+
 
 # gene_produce(gene_list) this must be run first
 # Agglomerative_cluster using this gene list to create clusters
